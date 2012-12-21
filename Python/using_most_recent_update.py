0fe2231e00dddb0c0346e2d2ae91c351cf8e4fd0
David Leen <dleen@uw.edu>
2012-12-21 12:46:49 +0100

    Found the most recent time updates

--- Python/using_most_recent_update.py
+++ Python/using_most_recent_update.py
@@ -1,39 +1,59 @@
-from numpy import genfromtxt, savetxt
 import numpy as np
-import csv
 import pandas as pd
 import leader_board as lb
+import re
 
 def main():
 	data_set_main = "PublicLeaderboardSet"
+	# Find the location of flighthistoryevents.csv for the leaderboard dataset:
 	fhe_location = lb.leader_board_data()
 
 	# Read in the flight_history_ids of the flight times to be predicted:
 	data_test_set = \
 		pd.read_csv("../Data/" + data_set_main + "/test_flights_combined.csv", parse_dates=True)
 
-	# Read in one of the training days:
+	# Read in one of the training days, specifically the file flighthistoryevents.csv:
 	data_train_set = \
 		pd.read_csv("../Data/" + fhe_location[0], parse_dates=True)
 
+	# Inner join the two data sets on flight_history_id:
+	df_join = pd.merge(left=data_test_set, right=data_train_set, on='flight_history_id')
 
-	df = data_train_set.groupby('flight_history_id')
+	# Group entries in table by flight_history_id:
+	df_grouped = df_join.groupby('flight_history_id')
+
+	# Select all entries corresponding to a particular fhid:
+	# TODO: replace this with a loop over all ids.
+	i = 0
+	for name, group in df_grouped:
+		if i < 100:
+			#print name
+			#print group
+			print most_recent_update(group)
+		else: 
+			break
+
+		i = i + 1
 
 
-	test = df.groups[281417384]
-	
-	test2 = data_train_set.ix[test]
+def most_recent_update(common_fh_id):
+	val2 = common_fh_id.sort_index(by='date_time_recorded', ascending=False)
 
-	test3 = test2['event'] == "Time Adjustment"
-	print test3
+	for row in val2['data_updated']:
+		if "ERA" in row:
+			#print row
+			era = re.search('(ERA-.*?(?<=New=))(?P<dt>\d\d/\d\d/\d\d \d\d:\d\d)', row)
+			#print era.group('dt')
+			break
 
-	print test2.sort_index(by='date_time_recorded')
+	for row in val2['data_updated']:
+		if "EGA" in row:
+			#print row
+			ega = re.search('(EGA-.*?(?<=New=))(?P<dt>\d\d/\d\d/\d\d \d\d:\d\d)', row)
+			#print ega.group('dt')
+			break
 
-def most_recent_update(grouped_vals):
-	val = grouped_vals.sort_index(by='date_time_recorded')
-
-
-
+	return [era.group('dt'), ega.group('dt')]
 
 if __name__=='__main__':
  	main()
